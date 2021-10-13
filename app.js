a68039fa437b036ce4905b16324e316f27cdeec1
SmartGlaxx <52786384+SmartGlaxx@users.noreply.github.com>
2021-10-13 08:27:43 +0100

    Add CORS workaround

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ const app = express()
 app.use(urlencoded({extended : false}))
 app.use(express.json())
 
+app.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, Content, Accept, Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.sendStatus(200);
+});
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/users', usersRouter)
 app.use('/api/v1/invest', investRouter)
